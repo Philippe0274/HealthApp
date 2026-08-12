1e63fe7d5ddca736c911805a18e54153726f95dc
Philippe0274 <philippe.demeersman1970@gmail.com>
2026-08-12 23:22:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2.1.16';
+const APP_VERSION = '3.0.0';
 const CACHE_PREFIX = 'health-tracker-cache-';
 const CACHE_NAME = CACHE_PREFIX + APP_VERSION;
 const APP_BASE = new URL(self.registration.scope).pathname;
